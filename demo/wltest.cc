f839ed671989e01e2d8475db6c231f5759267738
pointer-to-bios <pointer-to-bios@outlook.com>
2022-12-01 22:47:32 +0800

    add demo1-wltest

--- demo/wltest.cc
+++ demo/wltest.cc
@@ -3,13 +3,50 @@
 #include <wayland-client.h>
 #include <wayland-server.h>
 
+#include <unistd.h>
+
+void on_global_added(void *data,
+                     struct wl_registry *wl_registry,
+                     uint32_t name,
+                     const char *interface,
+                     uint32_t version)
+{
+    (void)data;
+    (void)wl_registry;
+    std::cerr << " Global added: " << interface << ", v" << version
+              << " (name " << name << ")"
+              << std::endl;
+}
+
+void on_global_removed(void *data,
+                       struct wl_registry *wl_registry,
+                       uint32_t name)
+{
+    (void)data;
+    (void)wl_registry;
+    std::cerr << " Global removed: name: " << name
+              << std::endl;
+}
+
+wl_registry_listener registry_listener = {
+    .global = on_global_added,
+    .global_remove = on_global_removed
+};
+
 int main()
 {
     wl_display *display = wl_display_connect(0);
+
+    //获取registry对象用于注册其他全局对象
     wl_registry *registry = wl_display_get_registry(display);
-    wl_registry_listener *registry_listener;
-    wl_registry_add_listener(registry, registry_listener, nullptr);
+
+    //注册事件监听回调函数
+    wl_registry_add_listener(registry, &registry_listener, nullptr);
 
     wl_display_dispatch(display);
     wl_display_roundtrip(display);
+
+    sleep(5);
+
+    wl_display_disconnect(display);
 }
