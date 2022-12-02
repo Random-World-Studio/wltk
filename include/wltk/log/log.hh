147e555ca80a51d9b4629137c184f5124202dfc1
pointer-to-bios <pointer-to-bios@outlook.com>
2022-12-02 17:05:31 +0800

    add global logger obj

---
+++ include/wltk/log/log.hh
@@ -0,0 +1,20 @@
+/**
+ * @file log.hh
+ * @author pointer-to-bios (pointer-to-bios@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-02
+ *
+ * @copyright Copyright (c) 2022 Random World Studio
+ *
+ */
+
+#ifndef __log_hh__
+#define __log_hh__
+
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+extern std::shared_ptr<spdlog::logger> logger;
+
+#endif
---
+++ src/log/log.cc
@@ -0,0 +1,15 @@
+/**
+ * @file log.cc
+ * @author pointer-to-bios (pointer-to-bios@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-02
+ * 
+ * @copyright Copyright (c) 2022 Random World Studio
+ * 
+ */
+
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+std::shared_ptr<spdlog::logger> logger;
